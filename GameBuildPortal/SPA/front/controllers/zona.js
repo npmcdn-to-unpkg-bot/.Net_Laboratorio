06a3a77b8a1a1f58254c84d9a805519a6a720a92
Andres Botta <andresbotta@gmail.com>
2016-05-30 16:16:47 -0300

    agregado modal en zonas

--- GameBuildPortal/SPA/front/controllers/zona.js
+++ GameBuildPortal/SPA/front/controllers/zona.js
@@ -3,7 +3,19 @@
     angular.module('atlas2-juego').controller('zonaCtrl', ['$scope', zonaCtrl]);
 
     function zonaCtrl($scope) {
-        
+        $scope.iteraccion = null;
+
+        $scope.mostrarIteraccion = function(){
+            $('#modal-iteraccion').modal('show');
+            console.log('show');
+        }
+
+        $scope.guardarIteraccion = function () {
+            console.log(this.iteraccion);
+
+            $('#modal-iteraccion').modal('hide');
+        }
+
     }
 
 })();
